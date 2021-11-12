3e8494c85c6b5457b8671f6cff2a4dda899fc69f
Ida Ekmark <ekmark@student.chalmers.se>
2021-11-12 18:36:54 +0100

    Fixed conflicts

--- E2/E2code3.c
+++ E2/E2code3.c
@@ -36,7 +36,7 @@ void calc_acc(double *a, double *u, int size_of_u, double alpha)
     /* Calculating the acceleration of the inner points */
     for (i = 1; i < size_of_u - 1; i++){
         a[i] = (u[i - 1] - 2*u[i] + u[i + 1]) 
-        		+ alpha * ( (u[i+1] - u[i] * (u[i+1] - u[i]) 
+        		+ alpha * ( (u[i+1] - u[i]) * (u[i+1] - u[i]) 
         				- (u[i] - u[i-1]) * (u[i] - u[i-1]) );
     }
 }
@@ -253,13 +253,8 @@ int main()
 	
 	double *time_array;
 	time_array = malloc((n_t + 1) * sizeof (double));
-<<<<<<< HEAD:E2/E2code2.c
-	arange(time_array, 0, n_t, dt);
-	saveqQpPtoFile("3/qQpP_0.01.csv", time_array, q, Q, p, P, n_t, n_p);
-=======
 	arange(time_array, 0, n_t+1, dt);
-	saveqQpPtoFile("3/qQpP_alpha1e-1.csv", time_array, q, Q, p, P, n_t, n_p);
->>>>>>> 8bdc0d6a14f6ee9df64f23af97506927e0469662:E2/E2code3.c
+	saveqQpPtoFile("3/qQpP_0.01.csv", time_array, q, Q, p, P, n_t, n_p);
 	
 	for (int i=0; i < n_t+1; i++){
 		free(q[i]);
@@ -289,11 +284,7 @@ int main()
 	}
 
     calculateEnergy(n_t, N_PARTICLES, Q, P, kinE, potE, totE);
-<<<<<<< HEAD:E2/E2code2.c
     saveKPEtoFile("3/KPE_0.01.csv", time_array, kinE, potE, totE, n_t, N_PARTICLES);
-=======
-    saveKPEtoFile("3/KPE_alpha1e-1.csv", time_array, kinE, potE, totE, n_t, N_PARTICLES);
->>>>>>> 8bdc0d6a14f6ee9df64f23af97506927e0469662:E2/E2code3.c
 
     free(time_array);
 
