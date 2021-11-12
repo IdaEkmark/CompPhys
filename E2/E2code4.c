fc8ab60a5e49ae16a42d76368e97fcdaa2160bdb
Emil Ingelsten <emil.ingelsten@telia.com>
2021-11-12 18:29:14 +0100

    Added support for skipping timesteps

--- E2/E2code4.c
+++ E2/E2code4.c
@@ -31,7 +31,7 @@ void calc_acc(double *a, double *u, int size_of_u, double alpha)
     		+ alpha * ( (u[1] - u[0]) * (u[1] - u[0]) - u[0] * u[0] );
     a[size_of_u - 1] = (u[size_of_u - 2] - 2*u[size_of_u - 1]) 
     		+ alpha * ( u[size_of_u - 1] * u[size_of_u - 1] 
-											 - (u[size_of_u - 1] - u[size_of_u - 2]) * (u[size_of_u - 1] - u[size_of_u - 2]));
+			- (u[size_of_u - 1] - u[size_of_u - 2]) * (u[size_of_u - 1] - u[size_of_u - 2]));
     
     /* Calculating the acceleration of the inner points */
     for (i = 1; i < size_of_u - 1; i++){
@@ -152,14 +152,14 @@ void arange(double *array, double start, int len_t, double dt){
 
 
 void calculateEnergy(int n_timesteps, int n_particles, double **Q, double **P,
-        double **kinE, double **potE, double **totE) 
+        double **kinE, double **potE, double **totE, int skipsize) 
 {
     double wSq;
-	for (int t = 0; t < n_timesteps + 1; t++) { 
+	for (int t = 0; t < n_timesteps/skipsize + 1; t++) { 
 		for (int p = 0; p < n_particles; p++) {
             wSq = 4 * sin( (p+1)*PI /  ((double) 2*(n_particles + 1)) )*sin( (p+1)*PI / ((double) 2*(n_particles + 1)) );
-			kinE[t][p] = 0.5 * P[t][p]*P[t][p];
-            potE[t][p] = 0.5 * wSq * Q[t][p]*Q[t][p];
+			kinE[t][p] = 0.5 * P[skipsize*t][p]*P[skipsize*t][p];
+            potE[t][p] = 0.5 * wSq * Q[skipsize*t][p]*Q[skipsize*t][p];
             totE[t][p] = kinE[t][p] + potE[t][p];
 		}
 	}
@@ -197,7 +197,8 @@ void saveKPEtoFile(char *fname, double *time_array,
 
 int main()
 {
-	double t_max = 25000.0; int n_t = 250000; double dt = t_max / (double) n_t; int n_p = N_PARTICLES; double alpha = 0.01;
+	double t_max = 1e6; int n_t = 10000000; double dt = t_max / (double) n_t; 
+	int skipsize = 1000; int n_p = N_PARTICLES; double alpha = 0.1;
 	double trans_matrix[n_p][n_p];
     double **q;
     double **Q;
@@ -244,15 +245,10 @@ int main()
 	
 	velocity_verlet(n_t, n_p, p, q, dt, alpha);
 	
-	for (int t = 1; t < n_t; t++){
+	for (int t = 1; t < n_t+1; t++){
 		transform_to_normal_modes(trans_matrix, n_p, q[t], Q[t]);
 		transform_to_normal_modes(trans_matrix, n_p, p[t], P[t]);
 	}
-	
-	double *time_array;
-	time_array = malloc((n_t + 1) * sizeof (double));
-	arange(time_array, 0, n_t, dt);
-	saveqQpPtoFile("3/qQpP.csv", time_array, q, Q, p, P, n_t, n_p);
 	
 	for (int i=0; i < n_t+1; i++){
 		free(q[i]);
@@ -268,37 +264,41 @@ int main()
     double **potE;
     double **totE;
 
-    kinE = malloc((n_t + 1) * sizeof *kinE);
-	for (int i=0; i < n_t + 1; i++){
+    kinE = malloc((n_t/skipsize + 1) * sizeof *kinE);
+	for (int i=0; i < n_t/skipsize + 1; i++){
 		kinE[i] = malloc(n_p * sizeof *kinE[i]);
 	}
-	potE = malloc((n_t + 1) * sizeof *potE);
-	for (int i=0; i < n_t + 1; i++){
+	potE = malloc((n_t/skipsize + 1) * sizeof *potE);
+	for (int i=0; i < n_t/skipsize + 1; i++){
 		potE[i] = malloc(n_p * sizeof *potE[i]);
 	}
-	totE = malloc((n_t + 1) * sizeof *totE);
-	for (int i=0; i < n_t + 1; i++){
+	totE = malloc((n_t/skipsize + 1) * sizeof *totE);
+	for (int i=0; i < n_t/skipsize + 1; i++){
 		totE[i] = malloc(n_p * sizeof *totE[i]);
 	}
+	
+	double *time_array;
+	time_array = malloc((n_t/skipsize + 1) * sizeof (double));
+	arange(time_array, 0, n_t/skipsize + 1, skipsize*dt);
 
-    calculateEnergy(n_t, N_PARTICLES, Q, P, kinE, potE, totE);
-    saveKPEtoFile("3/KPE.csv", time_array, kinE, potE, totE, n_t, N_PARTICLES);
+    calculateEnergy(n_t, N_PARTICLES, Q, P, kinE, potE, totE, skipsize);
+    saveKPEtoFile("4/KPE_1e-1.csv", time_array, kinE, potE, totE, n_t/skipsize, N_PARTICLES);
 
     free(time_array);
-
-    for (int i=0; i < n_t+1; i++){
+	
+    for (int i=0; i < n_t/skipsize+1; i++){
 		free(kinE[i]);
 	}
 	free(kinE);
-	for (int i=0; i < n_t+1; i++){
+	for (int i=0; i < n_t/skipsize+1; i++){
 		free(potE[i]);
 	}
 	free(potE);
-    for (int i=0; i < n_t+1; i++){
+    for (int i=0; i < n_t/skipsize+1; i++){
 		free(totE[i]);
 	}
 	free(totE);
-
+	
     for (int i=0; i < n_t+1; i++){
 		free(Q[i]);
 	}
--- E2/plotEnergies.py
+++ E2/plotEnergies.py
@@ -9,20 +9,21 @@ import matplotlib.pyplot as plt
 # skip_header skips the first
 # row in data.csv
 filename = "KPE"
-array = np.genfromtxt('2/' + filename + '.csv', delimiter=',', skip_header=1)
+folder = '4'
+array = np.genfromtxt(folder + '/' + filename + '_1e-1.csv', delimiter=',', skip_header=1)
 modeNums = [1, 2, 3, 4, 5]
-maxtIndex = 2500
+maxtIndex = -2
 
 fig, ax = plt.subplots(figsize=(11,7))
 for k in modeNums:
-    ax.plot(array[:maxtIndex + 1, 0], array[:maxtIndex + 1, 1 + 3*(k-1)], label = "Kinetic energy, mode " + str(k))
-    ax.plot(array[:maxtIndex + 1, 0], array[:maxtIndex + 1, 2 + 3*(k-1)], label = "Potential energy, mode " + str(k))
+    #ax.plot(array[:maxtIndex + 1, 0], array[:maxtIndex + 1, 1 + 3*(k-1)], label = "Kinetic energy, mode " + str(k))
+    #ax.plot(array[:maxtIndex + 1, 0], array[:maxtIndex + 1, 2 + 3*(k-1)], label = "Potential energy, mode " + str(k))
     ax.plot(array[:maxtIndex + 1, 0], array[:maxtIndex + 1, 3 + 3*(k-1)], label = "Total energy, mode " + str(k))
 
-ax.set_xlabel('Time (ps)')
-ax.set_ylabel('Energy (eV)')
-ax.set_title('$\Delta t = 0.1$')
+ax.set_xlabel('Time (FPUT units)')
+ax.set_ylabel('Energy (FPUT units)')
+ax.set_title('$\\alpha = 0.1$')
 ax.legend(loc='best')
 ax.grid()
 
-fig.savefig('2/' + filename + '_1to5.pdf')
+fig.savefig(folder + '/' + 'E' + '_1to5_1e-1.pdf')
