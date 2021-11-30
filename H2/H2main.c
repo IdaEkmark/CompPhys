d2ec0d392f97e05ccba9f25801fac46ba90294a6
Ida Ekmark <ekmark@student.chalmers.se>
2021-11-30 15:28:57 +0100

    Done with getNewConfig, evalWaveFun, evalLocalEnergy, evalDensityNormalization, evalDensityFun

--- H2/H2main.c
+++ H2/H2main.c
@@ -3,29 +3,72 @@
 #include <stdlib.h>
 #include <gsl/gsl_rng.h>
 
+#define PI 3.141592653589
+
 void getNewConfig(double *R_m, double *R_t, double d, gsl_rng * r) {
 	int coordinate = floor(gsl_rng_uniform(r)*6);
-	for (int i=0; i<6; i++){
-		if (i == coordinate) {
-			R_t[i] = R_m[i] + 2*d*gsl_rng_uniform(r) - d;
+	for (int j=0; j<6; j++){
+		if (j == coordinate) {
+			R_t[j] = R_m[j] + 2*d*gsl_rng_uniform(r) - d;
 		} else {
-			R_t[i] = R_m[i];
+			R_t[j] = R_m[j];
 		}
 	}
 	
 }
 
-double metropolis(double d, gsl_rng * r, double alpha, int N){
+double evalWaveFun(double *R, double alpha) {
+    double r1 = sqrt(R[0]*R[0] + R[1]*R[1] + R[2]*R[2]);
+    double r2 = sqrt(R[3]*R[3] + R[4]*R[4] + R[5]*R[5]);
+    double r12 = sqrt( (R[0]-R[3])*(R[0]-R[3]) + (R[1]-R[4])*(R[1]-R[4]) + (R[2]-R[5])*(R[2]-R[5]) );
+
+    return exp(-2.0*(r1+r2) + 1.0 / (2.0*( 1.0/r12 + alpha )) );
+}
+
+double evalLocalEnergy(double *R, double alpha) {
+    double r1 = sqrt(R[0]*R[0] + R[1]*R[1] + R[2]*R[2]);
+    double r2 = sqrt(R[3]*R[3] + R[4]*R[4] + R[5]*R[5]);
+    double r12 = sqrt( (R[0]-R[3])*(R[0]-R[3]) + (R[1]-R[4])*(R[1]-R[4]) + (R[2]-R[5])*(R[2]-R[5]) );
+    double denom = 1 + alpha * r12;
+    double dotProd = (R[0]/r1 - R[3]/r2) * (R[0]-R[3]) + (R[1]/r1 - R[4]/r2) * (R[1]-R[4]) + (R[2]/r1 - R[5]/r2) * (R[2]-R[5]);
+
+    return -4.0 + dotProd / (r12 * denom*denom) - 1.0/(r12 * denom*denom*denom) - 1.0/(4.0 * denom*denom*denom*denom) + 1/r12;
+}
+
+double evalDensityNormalization(double alpha, int N_norm, double d, gsl_rng * r) {
+	double I = 0; double g_i; double psi_i; double R_i[6]; double xi_i; 
+	for (int i=0; i<N_norm; i++) {
+		for (int j=0; j<6; j++) {
+			xi_i = 2*gsl_rng_uniform(r);
+			R_i[j] = 2*10*d/PI * acos(1 - xi_i) - 10*d;
+		}
+		psi_i = evalWaveFun(R_i, alpha);
+		g_i = psi_i * psi_i;
+		for (int j=0; j<6; j++) {
+			g_i *= 2/PI / sin(PI * R_i[j]); 
+		}
+		I += g_i;
+	}
+	I /= N_norm;
+	return I;
+}
+
+double evalDensityFunction(double *R, double alpha, double densityNorm) {
+	double psi = evalWaveFun(R, alpha);
+	double rho = psi * psi / densityNorm;
+	return rho;
+}
+
+double metropolis(double d, gsl_rng * r, double alpha, int N, int N_norm) {
 	double R_m[6] = {0, 0, 0, 0, 0, 0}; double R_t[6];
-	getNewConfig(R_m, R_t, d, r);
+	double densityNorm = evalDensityNormalization(alpha, N_norm, d, r);
+	
+	double p_m = evalDensityFunction(R_m, alpha, densityNorm); double p_t;
 	/*
-	double p_m = evalWeightFunction3(x_m, y_m, z_m); double p_t;
 	double I = 0; double normfactor = 1/(N * fraction_use);
 	int counter = 0;
 	for (int i = 0; i < N; i++) {
-		x_t = x_m + delta * (gsl_rng_uniform(r) - 0.5);
-		y_t = y_m + delta * (gsl_rng_uniform(r) - 0.5); 
-		z_t = z_m + delta * (gsl_rng_uniform(r) - 0.5);
+		getNewConfig(R_m, R_t, d, r);
 		
 		p_t = evalWeightFunction3(x_t, y_t, z_t);
 		if (gsl_rng_uniform(r) < p_t/p_m) {
@@ -46,8 +89,8 @@ double metropolis(double d, gsl_rng * r, double alpha, int N){
 
 int main(){
 	gsl_rng * r = gsl_rng_alloc(gsl_rng_mt19937); 
-	gsl_rng_set(r, 12); double d = 1.0; double alpha = 0.1; int N = 10000;
-	double k = metropolis(d, r, alpha, N);
+	gsl_rng_set(r, 12); double d = 1.0; double alpha = 0.1; int N = 10000; int N_norm = 10000;
+	double k = metropolis(d, r, alpha, N, N_norm);
 	
 	return 0;
 }
