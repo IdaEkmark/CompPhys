9e73c925a5d8cd7e5b3474f781779584486f4067
Ida Ekmark <ekmark@student.chalmers.se>
2021-11-30 14:34:25 +0100

    Can run

--- H2/H2main.c
+++ H2/H2main.c
@@ -3,15 +3,22 @@
 #include <stdlib.h>
 #include <gsl/gsl_rng.h>
 
-void getNewConfig(double *r, double d, gsl_rng * r) {
-	int coodinate = floor(gsl_rng_uniform(r)*6);
-	r[coordinate] += 2*d*gsl_rng_uniform(r) - d;
+void getNewConfig(double *R_m, double *R_t, double d, gsl_rng * r) {
+	int coordinate = floor(gsl_rng_uniform(r)*6);
+	for (int i=0; i<6; i++){
+		if (i == coordinate) {
+			R_t[i] = R_m[i] + 2*d*gsl_rng_uniform(r) - d;
+		} else {
+			R_t[i] = R_m[i];
+		}
+	}
+	
 }
 
-double metropolis(double delta, gsl_rng * r, int N, double fraction_use){
-	double x_m = 0; double x_t = x_m + delta * (gsl_rng_uniform(r) - 0.5);
-	double y_m = 0; double y_t = y_m + delta * (gsl_rng_uniform(r) - 0.5); 
-	double z_m = 0; double z_t = z_m + delta * (gsl_rng_uniform(r) - 0.5);
+double metropolis(double d, gsl_rng * r, double alpha, int N){
+	double R_m[6] = {0, 0, 0, 0, 0, 0}; double R_t[6];
+	getNewConfig(R_m, R_t, d, r);
+	/*
 	double p_m = evalWeightFunction3(x_m, y_m, z_m); double p_t;
 	double I = 0; double normfactor = 1/(N * fraction_use);
 	int counter = 0;
@@ -33,6 +40,14 @@ double metropolis(double delta, gsl_rng * r, int N, double fraction_use){
 			I += evalFunction3(x_m, y_m, z_m) * normfactor;
 		}
 	}
-	printf("accaptancerate = %.4f\n", (double)counter / N);
-	return I;
+	printf("accaptancerate = %.4f\n", (double)counter / N);*/
+	return 1.0;//I;
+}
+
+int main(){
+	gsl_rng * r = gsl_rng_alloc(gsl_rng_mt19937); 
+	gsl_rng_set(r, 12); double d = 1.0; double alpha = 0.1; int N = 10000;
+	double k = metropolis(d, r, alpha, N);
+	
+	return 0;
 }
